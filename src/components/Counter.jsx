3ebabf255d94649909a137f4c6304f8c12beb9b5
its-gio <me@its-g.io>
2019-01-04 15:07:03 -0800

    Adding new state tags

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3", "tag4"]
   };
 
   handleChange = () => {
